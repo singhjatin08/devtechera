80b434e8a831e59442af389139e9d62faf299a93
PHP Web Developer | Jatin Singh <146844794+singhjatin08@users.noreply.github.com>
2024-11-10 00:29:30 +0530

    Add files via upload

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/newIT/';
+    public string $baseURL = 'http://localhost/devtechera/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/about', 'Home::about');
+$routes->get('/services', 'Home::services');
+$routes->get('/portfolio', 'Home::portfolio');
+$routes->get('/blogs', 'Home::blogs');
 $routes->get('/contact', 'Home::contact');
