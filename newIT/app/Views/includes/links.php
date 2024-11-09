ca7b7c6f73024fb39933f767b3200a2fe05e3a11
Jatin Singh <gs.tem08@gmail.com>
2024-11-09 17:48:37 +0530

    codeignitor

---
+++ newIT/app/Views/includes/links.php
@@ -0,0 +1,41 @@
+<!-- favicons Icons -->
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url("public/assets/images/favicons/apple-touch-icon.png") ?>" />
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url("public/assets/images/favicons/favicon-32x32.png") ?>" />
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url("public/assets/images/favicons/favicon-16x16.png") ?>" />
+<link rel="manifest" href="<?php echo base_url("public//images/favicons/site.webmanifest") ?>" />
+<meta name="description" content="Zeinet HTML 5 Template " />
+
+<!-- fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com/">
+<link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&amp;display=swap"
+    rel="stylesheet">
+
+<link rel="stylesheet" href="<?php echo base_url("public/assets/vendors/bootstrap/css/bootstrap.min.css") ?>" />
+<link rel="stylesheet" href="<?php echo base_url("public/assets/vendors/animate/animate.min.css") ?>" />
+<link rel="stylesheet" href="<?php echo base_url("public/assets/vendors/animate/custom-animate.css") ?>" />
+<link rel="stylesheet" href="<?php echo base_url("public/assets/vendors/fontawesome/css/all.min.css") ?>" />
+<link rel="stylesheet" href="<?php echo base_url("public/assets/vendors/jquery-magnific-popup/jquery.magnific-popup.css") ?>" />
+<link rel="stylesheet" href="<?php echo base_url("public/assets/vendors/odometer/odometer.min.css") ?>" />
+<link rel="stylesheet" href="<?php echo base_url("public/assets/vendors/swiper/swiper.min.css") ?>" />
+<link rel="stylesheet" href="<?php echo base_url("public/assets/vendors/zeinet-icons/style.css") ?>">
+<link rel="stylesheet" href="<?php echo base_url("public/assets/vendors/zeinet-icons-two/style.css") ?>">
+<link rel="stylesheet" href="<?php echo base_url("public/assets/vendors/owl-carousel/owl.carousel.min.css") ?>" />
+<link rel="stylesheet" href="<?php echo base_url("public/assets/vendors/owl-carousel/owl.theme.default.min.css") ?>" />
+<link rel="stylesheet" href="<?php echo base_url("public/assets/vendors/timepicker/timePicker.css") ?>" />
+
+<!-- template styles -->
+<link rel="stylesheet" href="<?php echo base_url("public/assets/css/template.css") ?>" />
+<link rel="stylesheet" href="<?php echo base_url("public/assets/css/style.css") ?>" />
+
+<!-- <link rel="stylesheet" href="assets/vendors/jarallax/jarallax.css" />   -->
+<!-- <link rel="stylesheet" href="assets/vendors/nouislider/nouislider.min.css" />
+    <link rel="stylesheet" href="assets/vendors/nouislider/nouislider.pips.css" /> -->
+
+<!-- <link rel="stylesheet" href="assets/vendors/tiny-slider/tiny-slider.min.css" /> -->
+<!-- <link rel="stylesheet" href="assets/vendors/reey-font/stylesheet.css" /> -->
+
+<!-- <link rel="stylesheet" href="assets/vendors/bxslider/jquery.bxslider.css" /> -->
+<!-- <link rel="stylesheet" href="assets/vendors/bootstrap-select/css/bootstrap-select.min.css" /> -->
+<!-- <link rel="stylesheet" href="assets/vendors/vegas/vegas.min.css" /> -->
+<!-- <link rel="stylesheet" href="assets/vendors/jquery-ui/jquery-ui.css" /> -->
