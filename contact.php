3dc5811772f86747a868afa9b96dbd348952ca6a
Jatin Singh <gs.tem08@gmail.com>
2024-11-09 17:20:18 +0530

    first commit

---
+++ contact.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title> Contact Devtechera | Top Ranked Digital Marketing Company in India </title>
+
+    <?php
+    include("includes/links.php")
+        ?>
+
+</head>
+
+<body class="custom-cursor">
+    <?php
+    include("includes/header.php")
+        ?>
+
+    <!--Page Header Start-->
+    <section class="page-header">
+        <div class="page-header-bg" style="background-image: url(assets/images/backgrounds/page-header-bg.jpg)">
+        </div>
+        <div class="main-slider-border"></div>
+        <div class="main-slider-border main-slider-border-two"></div>
+        <div class="main-slider-border main-slider-border-three"></div>
+        <div class="main-slider-border main-slider-border-four"></div>
+        <div class="main-slider-border main-slider-border-five"></div>
+        <div class="main-slider-border main-slider-border-six"></div>
+        <div class="container">
+            <div class="page-header__inner">
+                <ul class="thm-breadcrumb list-unstyled">
+                    <li><a href="index.html">Home</a></li>
+                    <li><span>//</span></li>
+                    <li>Contact</li>
+                </ul>
+                <h2>Contact</h2>
+            </div>
+        </div>
+    </section>
+    <!--Page Header End-->
+
+    <!--Contact Page Start-->
+    <section class="contact-page">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-6 col-lg-6">
+                    <div class="contact-page__left">
+                        <div class="section-title text-left">
+                            <span class="section-title__tagline">contact with us</span>
+                            <h2 class="section-title__title">Ready to get in touch
+                                with zeinet</h2>
+                        </div>
+                        <p class="contact-page__text">Aliquam hendrerit a augue insuscipit. Etiam aliquam massa quis
+                            des mauris commodo venenatis ligula commodo leez sed blandit convallis dignissim onec
+                            vel pellentesque neque.
+                        </p>
+                        <ul class="list-unstyled contact-page__contact-list">
+                            <li>
+                                <div class="icon">
+                                    <span class="icon-phone-call"></span>
+                                </div>
+                                <div class="content">
+                                    <p>Have any question?</p>
+                                    <h4><a href="tel:8898006802">Free +88 (9800) 6802</a></h4>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="icon">
+                                    <span class="icon-message"></span>
+                                </div>
+                                <div class="content">
+                                    <p>Write email</p>
+                                    <h4><a href="mailto:needhelp@company.com">needhelp@company.com</a></h4>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="icon">
+                                    <span class="icon-placeholder"></span>
+                                </div>
+                                <div class="content">
+                                    <p>Visit anytime</p>
+                                    <h4>30 broklyn golden street. New York</h4>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6">
+                    <div class="contact-page__right">
+                        <div class="contact-page__content">
+                            <div class="contact-page__content-inner">
+                                <div class="contact-page-shape-1 float-bob-x">
+                                    <img src="assets/images/shapes/contact-page-shape-1.png" alt="">
+                                </div>
+                                <div class="section-title text-left">
+                                    <span class="section-title__tagline">contact us</span>
+                                    <h2 class="section-title__title">Write a message</h2>
+                                </div>
+                                <form action="assets/inc/sendemail.php"
+                                    class="contact-page__form contact-form-validated" novalidate="novalidate">
+                                    <div class="row">
+                                        <div class="col-xl-12">
+                                            <div class="contact-page__form-input-box">
+                                                <input type="text" placeholder="Your name" name="name">
+                                            </div>
+                                        </div>
+                                        <div class="col-xl-12">
+                                            <div class="contact-page__form-input-box">
+                                                <input type="email" placeholder="Email address" name="email">
+                                            </div>
+                                        </div>
+                                        <div class="col-xl-12">
+                                            <div class="contact-page__form-input-box">
+                                                <input type="text" placeholder="Phone number" name="phone">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-12">
+                                        <div class="contact-page__form-input-box text-message-box">
+                                            <textarea name="message" placeholder="Write a message"></textarea>
+                                        </div>
+                                        <div class="contact-page__btn-box">
+                                            <button type="submit" class="thm-btn contact-page__btn">Send a
+                                                message</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Contact Page End-->
+
+    <!--Google Map Start-->
+    <section class="google-map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4562.753041141002!2d-118.80123790098536!3d34.152323469614075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80e82469c2162619%3A0xba03efb7998eef6d!2sCostco+Wholesale!5e0!3m2!1sbn!2sbd!4v1562518641290!5m2!1sbn!2sbd"
+            class="google-map__one" allowfullscreen></iframe>
+
+    </section>
+    <!--Google Map End-->
+
+    <?php
+    include("includes/footer.php")
+        ?>
+
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,107 @@
+<div class="custom-cursor__cursor"></div>
+<div class="custom-cursor__cursor-two"></div>
+
+<div class="preloader">
+    <div class="preloader__image"></div>
+</div>
+<!-- /.preloader -->
+
+
+<div class="page-wrapper">
+    <section class="main-header-topbar">
+        <div class="main-header-topbar__right"></div>
+        <div class="container">
+            <div class="main-header-topbar__wrapper">
+                <ul class="main-header-topbar__info">
+                    <li>
+                        <span class="main-header-topbar__info__icon"><i class="zeinet-icons-two-phone"></i></span>
+                        <a href="tel:008757845682">+91 9205329258</a>
+                    </li>
+                    <li>
+                        <span class="main-header-topbar__info__icon"><i class="zeinet-icons-two-envelope"></i></span>
+                        <a href="mailto:zeinetinfo@gmail.com">info@devtechera.com</a>
+                    </li>
+                    <li>
+                        <span class="main-header-topbar__info__icon"><i class="zeinet-icons-two-location"></i></span>
+                        <a href="https://www.google.com/maps">Delhi NCR, India</a>
+                    </li>
+                </ul>
+                <div class="main-header-topbar__social">
+                    <h5 class="main-header-topbar__social__title">Follow Us:</h5>
+                    <a href="https://facebook.com/">
+                        <i class="fab fa-facebook-f" aria-hidden="true"></i>
+                        <span class="sr-only">Facebook</span>
+                    </a>
+                    <a href="https://twitter.com/">
+                        <i class="fab fa-twitter" aria-hidden="true"></i>
+                        <span class="sr-only">Twitter</span>
+                    </a>
+                    <a href="https://pinterest.com/">
+                        <i class="fab fa-pinterest-p" aria-hidden="true"></i>
+                        <span class="sr-only">Pinterest</span>
+                    </a>
+                    <a href="https://instagram.com/">
+                        <i class="fab fa-instagram" aria-hidden="true"></i>
+                        <span class="sr-only">Instagram</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <header class="main-header-five">
+        <nav class="main-menu">
+            <div class="container">
+                <div class="main-menu__wrapper clearfix">
+                    <div class="main-menu__left">
+                        <div class="main-menu__logo">
+                            <a href="/">
+                                <!-- <img src="assets/images/resources/logo-5.png" alt="zeinet" width="129"> -->
+                                 <span class="fw-bold">DevTechEra</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="main-menu__main-menu-box">
+                        <ul class="main-menu__list">
+                            <li class="active menu_item">
+                                <a href="/">Home</a> 
+                            </li>
+                            <li class="menu_item">
+                                <a href="about.html">About</a>
+                            </li>
+
+                            <li class="dropdown menu_item">
+                                <a href="#">Services</a>
+                                <ul class="shadow-box">
+                                    <li><a href="services.html">Web Design & Development</a></li>
+                                    <li><a href="services-carousel.html">Graphic Designing</a></li>
+                                    <li><a href="broadband.html">Video Editing</a></li>
+                                    <li><a href="tv-streaming.html">Digital Marketing</a></li>
+                                    <li><a href="all-for-mobile.html">ERP & Software</a></li>
+                                </ul>
+                            </li>
+
+                            <li class="menu_item">
+                                <a href="#">Portfolio</a>
+                            </li>
+                            
+                            <li class="menu_item">
+                                <a href="#">Blogs</a>
+                            </li>
+                            
+                            <li class="menu_item">
+                                <a href="contact.html">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="main-header-five__right">
+                        <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
+                        <a href="cart.html" class="main-menu__search"><i class="zeinet-icons-two-cart"></i></a>
+                        <a class="thm-btn thm-btn--two" href="contact.html">Get a Free Quote<span></span></a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <div class="stricky-header stricked-menu main-menu main-header-five">
+        <div class="sticky-header__content"></div><!-- /.sticky-header__content -->
+    </div><!-- /.stricky-header -->
