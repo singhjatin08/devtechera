2994dd5766700b2c88728ad4ca5ca4bd8b979834
PHP Web Developer | Jatin Singh <146844794+singhjatin08@users.noreply.github.com>
2024-11-10 00:28:45 +0530

    Add files via upload

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,6 +8,22 @@ class Home extends BaseController
     {
         return view('index');
     }
+    public function about(): string
+    {
+        return view('about');
+    }
+    public function services(): string
+    {
+        return view('services');
+    }
+    public function blogs(): string
+    {
+        return view('blogs');
+    }
+    public function portfolio(): string
+    {
+        return view('portfolio');
+    }
     public function contact(): string
     {
         return view('contact');
